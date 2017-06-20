1da6bd103e689bfbf0ada053cd25b4dbb66581f0
Alexander Shapoval <ashapoms@hotmail.com>
2017-06-20 20:03:20 +0300

    Nslookup function updated

--- open-wb-infra/open-wb-infra/PS/IISOld.ps1
+++ open-wb-infra/open-wb-infra/PS/IISOld.ps1
@@ -36,11 +36,49 @@ configuration IISInstall
 
 				## Get VM Public IP
 
+				# Function to resolve VM public fqdn 
+				function Resolve-DnsName2008
+				{
+					Param
+					(
+						[Parameter(Mandatory=$true)]
+						[string]$Name,
+						[string]$Server = '8.8.8.8'
+					)
+					Try
+					{
+						$nslookup = &nslookup.exe $Name $Server
+						$regexipv4 = "^(?:(?:0?0?\d|0?[1-9]\d|1\d\d|2[0-5][0-5]|2[0-4]\d)\.){3}(?:0?0?\d|0?[1-9]\d|1\d\d|2[0-5][0-5]|2[0-4]\d)$"
+
+						$name = @($nslookup | Where-Object { ( $_ -match "^(?:Name:*)") }).replace('Name:','').trim()
+
+						$deladdresstext = $nslookup -replace "^(?:^Address:|^Addresses:)",""
+						$Addresses = $deladdresstext.trim() | Where-Object { ( $_ -match "$regexipv4" ) }
+
+						$total = $Addresses.count
+						$AddressList = @()
+						for($i=1;$i -lt $total;$i++)
+						{
+							$AddressList += $Addresses[$i].trim()
+						}
+
+						$AddressList | %{
+
+						new-object -typename psobject -Property @{
+							Name = $name
+							IPAddress = $_
+							}
+						}
+					}
+					catch 
+					{ }
+				}
+
 				# Get VM public fqdn   
 				$vmPublicName = $env:COMPUTERNAME + ".westeurope.cloudapp.azure.com"
 
 				# Get VM public IP using function 
-				$vmPublicIP = ([System.Net.Dns]::GetHostAddresses($vmPublicName)).IPAddressToString
+				$vmPublicIP = (Resolve-DNSName2008 -Name $vmPublicName).IPAddress
 				$extIP = $vmPublicIP.Substring(0,$vmPublicIP.Length)
 
 
