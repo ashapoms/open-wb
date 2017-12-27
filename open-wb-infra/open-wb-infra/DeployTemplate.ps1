32107335c7803f4166e3cedbe1f3546bc3919c14
Alexander Shapoval <ashapoms@hotmail.com>
2017-12-27 09:02:01 +0300

    Paths changed

--- open-wb-infra/open-wb-infra/DeployTemplate.ps1
+++ open-wb-infra/open-wb-infra/DeployTemplate.ps1
@@ -4,8 +4,8 @@
 # The script creates resource group and deploy ARM template
 #
 
-$aadpass = ConvertTo-SecureString "DMSVonashu26.@3!" -AsPlainText -Force
-$aadcred = New-Object System.Management.Automation.PSCredential ("admin@contosomsspb.onmicrosoft.com", $aadpass)
+$aadpass = ConvertTo-SecureString "<Azure admin password>" -AsPlainText -Force
+$aadcred = New-Object System.Management.Automation.PSCredential ("<admin login>@<tenant name>.onmicrosoft.com", $aadpass)
 
 #Add-AzureRmAccount
 Login-AzureRmAccount -Credential $aadcred
@@ -13,23 +13,8 @@ $DeployIndex = "201"
 $ResourceGroupName = "Open-RG" + $DeployIndex
 $ResourceGroupLocation = "westeurope"
 $OpenDeploymentName = "Open-Dep" + $DeployIndex
-$TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\openwbVMs.json"
-$TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\openwbVMs.parameters.json"
-# $TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\sqlVM.json"
-# $TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\sqlVM.parameters.json"
-# $TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\sqlMultiVMs.json"
-# $TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\sqlMultiVMs.parameters.json"
-# $TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\gateVMSS.json"
-# $TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\gateVMSS.parameters.json"
-# $TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\gateSS.json"
-# $TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\gateSS.parameters.json"
-# $TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\loadVM.json"
-# $TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\loadVM.parameters.json"
-# $TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\stubVM.json"
-# $TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\stubVM.parameters.json"
-
-# $TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\openWb.json"
-# $TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\openWb.parameters.json"
+$TemplateFile = "<Path to template>\<template file>.json"
+$TemplateParametersFile = "<Path to template parameters>\<template parameters file>.parameters.json"
 
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force
 
--- open-wb-infra/open-wb-infra/ParamDeployTemplate.ps1
+++ open-wb-infra/open-wb-infra/ParamDeployTemplate.ps1
@@ -6,8 +6,8 @@ Param(
 	[string] $ResourceGroupLocation = "westeurope",
 	[string] $DeployIndex = "01",
 	[string] $ResourceGroupPrefix = "Open-RG",
-	[string] $AzureUserName = "admin@contosomsspb.onmicrosoft.com",
-	[string] $AzureUserPassword = "DMSVonashu26.@3!"
+	[string] $AzureUserName = "<admin login>@<tenant name>.onmicrosoft.com",
+	[string] $AzureUserPassword = "<Azure admin password>"
 )
 
 $AadPass = ConvertTo-SecureString $AzureUserPassword -AsPlainText -Force
@@ -16,8 +16,8 @@ Login-AzureRmAccount -Credential $AadCred
 
 $ResourceGroupName = $ResourceGroupPrefix + $DeployIndex
 $DeploymentName = $ResourceGroupPrefix + "-Dep" + $DeployIndex
-$TemplateFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\openWb.json"
-$TemplateParametersFile = "C:\Users\ashapo\Work Folders\DPE\VS\OpenArm\open-wb\open-wb-infra\open-wb-infra\openWb.parameters.json"
+$TemplateFile = "<Path to template>\<template file>.json"
+$TemplateParametersFile = "<Path to template parameters>\<template parameters file>.parameters.json"
 
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force
 
